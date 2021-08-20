08bf9602803e80fe6233f2c2b7ae2003b79d4d5d
happyjosh-tech <okwajoshltd@gmail.com>
2021-08-20 16:26:10 -0700

    commit of changes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c033f9f2c075fba05fa685953849a0f01e6dbb9f',
+    'name' => 'josh/joshresume',
+  ),
+  'versions' => 
+  array (
+    'josh/joshresume' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c033f9f2c075fba05fa685953849a0f01e6dbb9f',
+    ),
+  ),
+);
