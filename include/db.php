b7bcb228241dadc03f7a9eced12e391ad51f0ce8
happyjosh-tech <okwajoshltd@gmail.com>
2021-08-20 15:20:03 -0700

    composer files

--- include/db.php
+++ include/db.php
@@ -5,3 +5,15 @@ $db=mysqli_connect("localhost","root","","joshresume");
 //}else{
 //    echo "something is wrong with database !";
 //}
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
